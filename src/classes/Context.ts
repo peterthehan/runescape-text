9abae1793bc7d56d9fb0aa43341dc078b58f756d
Peter Han <peter.han.9493@gmail.com>
2023-06-03 19:49:11 -0700

    Refactor Context again

--- src/classes/Context.ts
+++ src/classes/Context.ts
@@ -2,10 +2,9 @@ import { CanvasRenderingContext2D, createCanvas, registerFont } from "canvas";
 import { resolve } from "path";
 
 const path = resolve(__dirname, "../assets/runescape_uf.ttf");
-registerFont(path, {
-  family: "RuneScape",
-});
+registerFont(path, { family: "RuneScape" });
 
+const FONT_BASE_SIZE = 16;
 const FONT_NAME = "RuneScape";
 
 export class Context {
@@ -17,7 +16,7 @@ export class Context {
   }
 
   initialize() {
-    const fontSize = 16 * this.scale;
+    const fontSize = FONT_BASE_SIZE * this.scale;
     this.context.font = `${fontSize}px ${FONT_NAME}`;
     this.context.shadowColor = "black";
     this.context.shadowOffsetX = this.scale;
@@ -29,7 +28,7 @@ export class Context {
 
 export function measureText(message: string, scale: number) {
   const context = createCanvas(0, 0).getContext("2d");
-  const fontSize = 16 * scale;
+  const fontSize = FONT_BASE_SIZE * scale;
   context.font = `${fontSize}px ${FONT_NAME}`;
   const measurement = context.measureText(message);
 
